588c48ea5c6ec22b30112dda69d66bee2f254230
miriamsaucedo <miriam.saucedo@exos.mx>
2023-12-13 12:18:33 -0600

    fixes api routing

--- src/config/stores/axios.ts
+++ src/config/stores/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/",
+  baseURL: "https://donaciones-api.ddns.net/api/",
   headers: {
     "Content-Type": "application/json",
   },
