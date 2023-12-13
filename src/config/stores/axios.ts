d4dcb679abe75c45a7558327b2e042b50b1360d5
Roy Salgado <roy.salgado@exos.mx>
2023-12-13 00:46:46 -0600

    test

--- src/config/stores/axios.ts
+++ src/config/stores/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://52.70.89.161:3000/api/",
+  baseURL: "https://donaciones-api.ddns.net/api/",
   headers: {
     "Content-Type": "application/json",
   },
