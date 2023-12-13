cbd8f5aaa33ccb75d255b8f7544eba10f37f1018
miriamsaucedo <miriam.saucedo@exos.mx>
2023-12-12 21:29:44 -0600

    change api url

--- src/config/stores/axios.ts
+++ src/config/stores/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://52.70.89.161:3000/api/",
+  baseURL: "http://donaciones.ddns.net:3000/api/",
   headers: {
     "Content-Type": "application/json",
   },
