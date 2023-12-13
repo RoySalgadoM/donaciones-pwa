bc694b8e86e6f391624f1bd0c5226887d75ce2d5
Roy Salgado <roy.salgado@exos.mx>
2023-12-12 20:06:24 -0600

    update docker config

--- src/config/stores/axios.ts
+++ src/config/stores/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://b967hfr3-3000.usw3.devtunnels.ms/api/",
+  baseURL: "http://52.70.89.161:3000/api/",
   headers: {
     "Content-Type": "application/json",
   },
